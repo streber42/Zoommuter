8aed4446f0e9695a046c44fe6e9dfd9d483961d9
Sean Murphy <murphy@halfast.com>
2020-11-26 23:53:25 -0800

    working with screen off so its lower current

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,67 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
+#include <M5StickC.h>
+#include "AXP192.h"
+TFT_eSprite tftSprite = TFT_eSprite(&M5.Lcd);
 
 BleKeyboard bleKeyboard;
 boolean oldPinState = LOW;
 const int buttonPin = G37;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  // Serial.begin(115200);
+  // Serial.println("Starting BLE work!");
+  m5.begin(false);
+  // M5.Lcd.setRotation(3);
+  // tftSprite.createSprite(160, 80);
+  // tftSprite.setRotation(3);
+  M5.Axp.EnableCoulombcounter();
   bleKeyboard.begin();
   pinMode(buttonPin, INPUT_PULLDOWN);
 }
 
 void loop() {
-  if (bleKeyboard.isConnected()) {
-    if (digitalRead(buttonPin) == HIGH) {
-      //If the button pin is pressed (since it is pulled down, it is pressed when it is high
-      if (oldPinState == LOW) {
-        //if the old Pin state was LOW and the button pin is HIGH than...
-        //send the spacebar
-        bleKeyboard.print(" ");
-        Serial.println("BUTTON PRESSED");
+  // tftSprite.fillSprite(BLACK);
+  // tftSprite.setCursor(0, 0, 1);
+  // tftSprite.printf("AXP Temp: %.1fC \r\n", M5.Axp.GetTempInAXP192());
+  // tftSprite.setCursor(0, 10);
+  // tftSprite.printf("Bat:\r\n  V: %.3fv  I: %.3fma\r\n", M5.Axp.GetBatVoltage(), M5.Axp.GetBatCurrent());
+  // tftSprite.setCursor(0, 30);
+  // tftSprite.printf("USB:\r\n  V: %.3fv  I: %.3fma\r\n", M5.Axp.GetVBusVoltage(), M5.Axp.GetVBusCurrent());
+  // tftSprite.setCursor(0, 50);
+  // tftSprite.printf("5V-In:\r\n  V: %.3fv  I: %.3fma\r\n", M5.Axp.GetVinVoltage(), M5.Axp.GetVinCurrent());
+  // tftSprite.setCursor(0, 70);
+  // tftSprite.printf("Bat power %.3fmw", M5.Axp.GetBatPower());
+  // tftSprite.pushSprite(0, 0);
+  Serial.println(M5.Axp.GetVBusCurrent());
 
-        // gold
-        bleKeyboard.press(KEY_LEFT_SHIFT);
-        bleKeyboard.press(KEY_LEFT_GUI);
-        bleKeyboard.press('a');
-        bleKeyboard.releaseAll();
-        // bleKeyboard.println("AT+BleKeyboardCode=02-00-39-00-00-00-00"); // shift + caps lock
-        // ble.println("AT+BleKeyboardCode=02-00-04-00-00-00-00"); // shift + a
-        // ble.println("AT+BleKeyboardCode=00-00");
+  // 0x01 long press(1s), 0x02 press
+  if (M5.Axp.GetBtnPress() == 0x02)
+  {
+    esp_restart();
+  }
 
-        // if (ble.waitForOK())
-        // {
-        //   Serial.println(F("OK!"));
-        // }
-        // else
-        // {
-        //   Serial.println(F("FAILED!"));
-        // }
-
-        delay(500);        //or you can comment this one out, and it will send the newline character
-            //bleKeyboard.write(KEY_RETURN);
-      }
-      oldPinState = HIGH;
-    }
-    else {
-      oldPinState = LOW;
+  if (M5.BtnA.wasPressed())
+  {
+    // close tft voltage output
+    // M5.Axp.SetLDO2(false);
+    // close tft lcd voltage output
+    // M5.Axp.SetLDO3(false);
+    if (bleKeyboard.isConnected()) {
+      bleKeyboard.press(KEY_LEFT_SHIFT);
+      bleKeyboard.press(KEY_LEFT_GUI);
+      bleKeyboard.press('a');
+      bleKeyboard.releaseAll();
     }
   }
-  delay(10);
+
+  if (M5.BtnB.wasPressed()) {
+    M5.Axp.SetLDO2(false);
+    M5.Axp.SetLDO3(false);
+  }
+
+  M5.Axp.SetChargeCurrent(CURRENT_100MA);
+
+  M5.update();
+  delay(100);
 }
